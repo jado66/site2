996c2bdab6f31219a8d8acccc278a76c06523fb4
jado66 <77120464+jado66@users.noreply.github.com>
2023-11-16 11:01:30 -0700

    Hid marketing landing about view

--- src/sections/_marketing/view/marketing-landing-view.js
+++ src/sections/_marketing/view/marketing-landing-view.js
@@ -34,7 +34,7 @@ export default function MarketingLandingView() {
 
       <MarketingLandingServices />
 
-      <MarketingLandingAbout />
+      {/* <MarketingLandingAbout /> */}
 
       <MarketingLandingProcess />
 
