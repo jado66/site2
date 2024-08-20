02bdf5df454f93164c3c48edac7f10dd3589b9cc
Jadon Erwin <jadon.erwin@sinch.com>
2024-08-20 16:00:16 -0600

    Fixed mission error

--- src/sections/_marketing/about/marketing-about-mission-statement.js
+++ src/sections/_marketing/about/marketing-about-mission-statement.js
@@ -1,6 +1,6 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, Grid } from '@mui/material';
 
 export default function MarketingAboutMissionStatement() {
   return (
@@ -13,81 +13,78 @@ export default function MarketingAboutMissionStatement() {
         backgroundSize: 'cover', // Make sure the image covers the entire viewport
         backgroundPosition: 'center', // Center the image
         display: 'flex',
-        justifyContent: 'flex-start',
+        justifyContent: 'center',
         alignItems: 'center',
       }}
     >
       <Box
         sx={{
           position: 'absolute',
+          display: { lg: 'none', xs: 'block' },
           top: 0,
-          center: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black overlay
+        }}
+      />
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
           width: '100%',
           height: '100%',
           backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add a dark overlay for better contrast
           zIndex: 1,
         }}
       />
-      <Box
+      <Grid
+        container
         sx={{
           position: 'relative',
           zIndex: 2,
           color: 'white', // Adjust text color for readability
           padding: { xs: 2, md: 5 },
-          left: '10%',
-          width: '30%', // Take up the center 50% of the screen
+          display: 'flex',
+          flexDirection: 'row',
         }}
+        spacing={4}
       >
-        <Typography variant="h2" sx={{ textAlign: 'left' }}>
-          Our Mission
-        </Typography>
-
-        <Divider sx={{ my: 2, backgroundColor: 'primary.contrastText' }} />
-
-        <Typography
-          sx={{
-            my: 3,
-            mx: 0,
-            textAlign: 'left',
-          }}
-          variant="subtitle1"
-        >
-          We empower businesses with fully customized websites, tailored to their unique brand
-          needs.
-        </Typography>
-        <Typography
-          sx={{
-            my: 3,
-            mx: 0,
-            textAlign: 'left',
-          }}
-          variant="subtitle1"
-        >
-          With highly skilled top-tier developers leading the way, we leverage the latest
-          cutting-edge web technology to ensure modern and innovative web development solutions.
-        </Typography>
-        <Typography
-          sx={{
-            my: 3,
-            mx: 0,
-            textAlign: 'left',
-          }}
-          variant="subtitle1"
-        >
-          Our mission is to make our client partners stand out with an exceptional online presence
-          in the digital age.
-        </Typography>
-        <Typography
-          sx={{
-            my: 3,
-            mx: 0,
-            textAlign: 'left',
-          }}
-          variant="subtitle1"
-        >
-          At Platinum Programming, you can expect platinum-grade products, people, and partnerships.
-        </Typography>
-      </Box>
+        <Grid item lg={5} xs={12} sx={{ ml: 2 }}>
+          <Grid>
+            <Typography variant="h2" sx={{ textAlign: 'left' }}>
+              Our Mission
+            </Typography>
+            <Divider sx={{ my: 2, backgroundColor: 'primary.contrastText' }} />
+          </Grid>
+          <Grid item xs={12} mb={3}>
+            <Typography variant="subtitle1" sx={{ textAlign: 'left' }}>
+              We empower businesses with fully customized websites, tailored to their unique brand
+              needs.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} mb={3}>
+            <Typography variant="subtitle1" sx={{ textAlign: 'left' }}>
+              With highly skilled top-tier developers leading the way, we leverage the latest
+              cutting-edge web technology to ensure modern and innovative web development solutions.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} mb={3}>
+            <Typography variant="subtitle1" sx={{ textAlign: 'left' }}>
+              Our mission is to make our client partners stand out with an exceptional online
+              presence in the digital age.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" sx={{ textAlign: 'left' }}>
+              At Platinum Programming, you can expect platinum-grade products, people, and
+              partnerships.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item lg={7}></Grid>
+      </Grid>
     </Box>
   );
 }
