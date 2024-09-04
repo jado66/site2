48142f5968f62fc9a9684da8943f1354a48bdfa7
jadon-erwin-sinch <jadon.erwin@sinch.com>
2024-09-04 11:36:46 -0600

    Fixed Highly Skilled Dev Team

--- src/sections/_marketing/team/marketing-team-about.js
+++ src/sections/_marketing/team/marketing-team-about.js
@@ -13,24 +13,23 @@ import { useState } from 'react';
 // ----------------------------------------------------------------------
 
 export default function MarketingTeamAbout({ members }) {
+  const [laraeImage, setLaraeImage] = useState(1);
 
-  const [laraeImage, setLaraeImage] = useState(1)
-  
   const jdBioSections = jdBio.map((section, index) => (
-    <Typography variant = 'subtitle1' key = {`jdBio${index}`} gutterBottom mb = {2}>
+    <Typography variant="subtitle1" key={`jdBio${index}`} gutterBottom mb={2}>
       {section}
     </Typography>
-  ))
+  ));
 
   const laraeBioSections = laraeBio.map((section, index) => (
-    <Typography variant = 'subtitle1' key = {`laraeBio${index}`} gutterBottom mb = {2}>
+    <Typography variant="subtitle1" key={`laraeBio${index}`} gutterBottom mb={2}>
       {section}
     </Typography>
-  ))
+  ));
 
   const swapImage = () => {
-    setLaraeImage(p => (p+1)%2)
-  }
+    setLaraeImage((p) => (p + 1) % 2);
+  };
 
   return (
     <Container
@@ -38,8 +37,15 @@ export default function MarketingTeamAbout({ members }) {
         py: { xs: 10, md: 10 },
       }}
     >
-      <Typography variant="h2" sx={{ textAlign: 'center' }}>
-        Highly Skilled Development Team
+      <Typography
+        component="div"
+        variant="overline"
+        sx={{ color: 'text.disabled', textAlign: 'center' }}
+      >
+        Our Team
+      </Typography>
+      <Typography variant="h2" sx={{ textAlign: 'center', mt: 3 }}>
+        Highly Skilled Development&nbsp;Team
       </Typography>
 
       <Typography
@@ -57,47 +63,54 @@ export default function MarketingTeamAbout({ members }) {
       </Typography>
 
       <Grid container>
-
-        <Grid md = {4} xs = {12} px = {{xs:5, md:0}}>
-          <MarketingTeamItem key={members[0].id} member={members[0]} sx = {{maxHeight:{xs:'90vh', md:'auto'}}}/>
+        <Grid md={4} xs={12} px={{ xs: 5, md: 0 }}>
+          <MarketingTeamItem
+            key={members[0].id}
+            member={members[0]}
+            sx={{ maxHeight: { xs: '90vh', md: 'auto' } }}
+          />
         </Grid>
 
-        <Grid md = {8} xs = {12} p = {{xs:1, md:5}}>
+        <Grid md={8} xs={12} p={{ xs: 1, md: 5 }}>
           {jdBioSections}
         </Grid>
 
-        <Grid xs = {12} my = {6} display={{xs:'none', md:'block'}}>
-          <Divider/>
+        <Grid xs={12} my={6} display={{ xs: 'none', md: 'block' }}>
+          <Divider />
         </Grid>
 
-        <Grid md = {8} xs = {12} p = {{xs:1, md:5}} display={{xs:'none', md:'block'}}>
+        <Grid md={8} xs={12} p={{ xs: 1, md: 5 }} display={{ xs: 'none', md: 'block' }}>
           {laraeBioSections}
         </Grid>
-        <Grid md = {4} xs = {12} px = {{xs:5,  md:0}} mt = {{xs:5, md: 0}} onClick = {swapImage}>
-          <MarketingTeamItem key={members[laraeImage+1].id} member={members[laraeImage+1]} sx = {{maxHeight:{xs:'90vh', md:'auto'}}}/>
+        <Grid md={4} xs={12} px={{ xs: 5, md: 0 }} mt={{ xs: 5, md: 0 }} onClick={swapImage}>
+          <MarketingTeamItem
+            key={members[laraeImage + 1].id}
+            member={members[laraeImage + 1]}
+            sx={{ maxHeight: { xs: '90vh', md: 'auto' } }}
+          />
         </Grid>
-        <Grid md = {8} xs = {12} p = {{xs:1, md:5}} display={{xs:'block', md:'none'}}>
+        <Grid md={8} xs={12} p={{ xs: 1, md: 5 }} display={{ xs: 'block', md: 'none' }}>
           {laraeBioSections}
         </Grid>
 
-        <Grid xs = {12} my = {6} display={{xs:'none', md:'block'}}>
-        </Grid>
+        <Grid xs={12} my={6} display={{ xs: 'none', md: 'block' }}></Grid>
 
-        <Grid xs = {12} mb = {6} display={{xs:'none', md:'block'}}>
+        <Grid xs={12} mb={6} display={{ xs: 'none', md: 'block' }}>
           <Typography variant="h2" sx={{ textAlign: 'center' }}>
             Our Team
           </Typography>
         </Grid>
-          
-        <Grid xs = {12} display='flex' justifyContent='center' flexDirection='column'>
-          <img
-            src={'/assets/stock-photos/board.webp'}
-          
-            style={{borderRadius:'1em'}}
-          />
-          <Grid xs = {12} md = {10} mx  ='auto' mt = {3}>
-            <Typography variant='subtitle1'>
-              We offer a roster of 50+ contractors and employees, who have each been personally vetted by upper management.  Our roster includes senior software developers, lead engineers, doctoral graduates, subject matter experts, and other experienced professionals located in the Washington D.C area and throughout the United States.  It is our pleasure to serve you with a specialized team, involving professionals in web development, software, marketing, branding, design, and more.
+
+        <Grid xs={12} display="flex" justifyContent="center" flexDirection="column">
+          <img src={'/assets/stock-photos/board.webp'} style={{ borderRadius: '1em' }} />
+          <Grid xs={12} md={10} mx="auto" mt={3}>
+            <Typography variant="subtitle1">
+              We offer a roster of 50+ contractors and employees, who have each been personally
+              vetted by upper management. Our roster includes senior software developers, lead
+              engineers, doctoral graduates, subject matter experts, and other experienced
+              professionals located in the Washington D.C area and throughout the United States. It
+              is our pleasure to serve you with a specialized team, involving professionals in web
+              development, software, marketing, branding, design, and more.
             </Typography>
           </Grid>
         </Grid>
